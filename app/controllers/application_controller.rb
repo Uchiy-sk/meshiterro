60fa0153a09fa44a670f167b009a20a5d986200f
EC2 Default User <ec2-user@ip-172-31-33-119.ec2.internal>
2022-10-12 05:33:43 +0000

    [finish] 4-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # devise機能の前にメソッドを実行
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # 他のcontrollerからも参照可（ストロングパラメータ）
+  protected
+
+  def configure_permitted_parameters
+    # ユーザ名(name)のデータ操作許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
