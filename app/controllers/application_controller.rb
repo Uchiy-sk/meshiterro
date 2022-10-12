5334063eec74d23e49cffa1b106950c7fa18b911
EC2 Default User <ec2-user@ip-172-31-33-119.ec2.internal>
2022-10-12 06:40:36 +0000

    [finish] 4-7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
   # devise機能の前にメソッドを実行
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # サインイン後の遷移先メソッド（デフォルト:rootパス）
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+  # サインアウト後の遷移先メソッド（デフォルト:rootパス）
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
   # 他のcontrollerからも参照可（ストロングパラメータ）
   protected
 
